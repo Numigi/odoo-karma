726dc6ce6843196395b2d68d0144e23c5d6ff728
David Dufresne <david.dufresne@numigi.net>
2018-08-01 10:29:26 -0400

    Simplify code architecture and add tests for bisextile years and timezone cases

--- date_range_field_template/date_range.py
+++ date_range_field_template/date_range.py
@@ -1,138 +1,11 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import abc
 import pytz
 
-from datetime import date, datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models
-from typing import Tuple
-
-
-class AbstractRelativeDateComputer(metaclass=abc.ABCMeta):
-
-    @abc.abstractmethod
-    def compute(self, reference_date: datetime) -> datetime:
-        """Compute a date range given a date of reference.
-
-        The given reference_date parameter is expected to be expressed in the user's timezone.
-
-        The given date is not necessarily the current date.
-
-        :param reference_date: the date from which to compute the range.
-        :return: the date relative to the reference date
-        """
-        raise NotImplementedError()
-
-
-class BasicRelativeDateComputer(AbstractRelativeDateComputer):
-    """A date range that computes the dates based on relative time deltas."""
-
-    def __init__(
-        self, years: int = 0, months: int = 0, weeks: int = 0, days: int = 0,
-    ):
-        self._years = years
-        self._months = months
-        self._weeks = weeks
-        self._days = days
-
-    def compute(self, reference_date):
-        if self._years:
-            reference_date += relativedelta(years=self._years)
-
-        if self._months:
-            reference_date += relativedelta(months=self._months)
-
-        if self._weeks:
-            reference_date += relativedelta(weeks=self._weeks)
-
-        if self._days:
-            reference_date += relativedelta(days=self._days)
-
-        return reference_date
-
-
-class DateComputerDecorator(AbstractRelativeDateComputer):
-
-    def __init__(self, computer: AbstractRelativeDateComputer):
-        self._computer = computer
-
-
-class WeekStartDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        return reference_date - relativedelta(days=reference_date.isoweekday())
-
-
-class WeekEndDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        reference_date -= relativedelta(days=reference_date.isoweekday())
-        reference_date += relativedelta(weeks=1)
-        return reference_date - relativedelta(days=1)
-
-
-class MonthStartDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        return reference_date - relativedelta(days=reference_date.day - 1)
-
-
-class MonthEndDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        reference_date -= relativedelta(days=reference_date.day - 1)
-        reference_date += relativedelta(months=1)
-        return reference_date - relativedelta(days=1)
-
-
-class YearStartDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        reference_date -= relativedelta(days=reference_date.day - 1)
-        return reference_date - relativedelta(months=reference_date.month - 1)
-
-
-class YearEndDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        reference_date -= relativedelta(days=reference_date.day - 1)
-        reference_date -= relativedelta(months=reference_date.month - 1)
-        reference_date += relativedelta(months=12)
-        return reference_date - relativedelta(days=1)
-
-
-class DayStartDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        return reference_date - relativedelta(
-            hours=reference_date.hour,
-            minutes=reference_date.minute,
-            seconds=reference_date.second,
-            microseconds=reference_date.microsecond,
-        )
-
-
-class DayEndDateComputer(DateComputerDecorator):
-
-    def compute(self, reference_date):
-        reference_date = self._computer.compute(reference_date)
-        reference_date -= relativedelta(
-            hours=reference_date.hour,
-            minutes=reference_date.minute,
-            seconds=reference_date.second,
-            microseconds=reference_date.microsecond,
-        )
-        reference_date += relativedelta(days=1)
-        return reference_date - relativedelta(microseconds=1)
 
 
 class ComputedFieldDateRange(models.Model):
@@ -169,43 +42,118 @@ class ComputedFieldDateRange(models.Model):
     @api.multi
     def get_date_min(self) -> datetime:
         """Get the minimum date of the range relative to the current time."""
-        computer = BasicRelativeDateComputer(
-            years=self.year_min, months=self.month_min, weeks=self.week_min, days=self.day_min,
+        now = self._get_current_datetime()
+
+        date_min = now + relativedelta(
+            years=self.year_min,
+            months=self.month_min,
+            weeks=self.week_min,
+            days=self.day_min,
         )
 
         if self.week_start:
-            computer = WeekStartDateComputer(computer)
+            date_min = get_week_start(date_min)
 
         elif self.month_start:
-            computer = MonthStartDateComputer(computer)
+            date_min = get_month_start(date_min)
 
         elif self.year_start:
-            computer = YearStartDateComputer(computer)
+            date_min = get_year_start(date_min)
 
-        computer = DayStartDateComputer(computer)
+        else:
+            date_min = get_day_start(date_min)
 
-        return computer.compute(self._get_current_datetime())
+        return date_min
 
     @api.multi
     def get_date_max(self) -> datetime:
         """Get the maximum date of the range relative to the current time."""
-        computer = BasicRelativeDateComputer(
-            years=self.year_max, months=self.month_max, weeks=self.week_max, days=self.day_max,
+        now = self._get_current_datetime()
+
+        date_max = now + relativedelta(
+            years=self.year_max,
+            months=self.month_max,
+            weeks=self.week_max,
+            days=self.day_max,
         )
 
         if self.week_end:
-            computer = WeekEndDateComputer(computer)
+            date_max = get_week_end(date_max)
 
         elif self.month_end:
-            computer = MonthEndDateComputer(computer)
+            date_max = get_month_end(date_max)
 
         elif self.year_end:
-            computer = YearEndDateComputer(computer)
+            date_max = get_year_end(date_max)
 
-        computer = DayEndDateComputer(computer)
+        else:
+            date_max = get_day_end(date_max)
 
-        return computer.compute(self._get_current_datetime())
+        return date_max
 
     def _get_current_datetime(self):
         tz = pytz.timezone(self.env.context.get('tz') or 'UTC')
         return datetime.now(tz)
+
+
+def get_day_start(date_: datetime):
+    """Get the start of the day relative to a given date."""
+    return date_ - relativedelta(
+        hours=date_.hour,
+        minutes=date_.minute,
+        seconds=date_.second,
+        microseconds=date_.microsecond,
+    )
+
+
+def get_day_end(date_: datetime):
+    """Get the end of the day relative to a given date."""
+    date_ -= relativedelta(
+        hours=date_.hour,
+        minutes=date_.minute,
+        seconds=date_.second,
+        microseconds=date_.microsecond,
+    )
+    date_ += timedelta(1)
+    return date_ - relativedelta(microseconds=1)
+
+
+def get_week_start(date_: datetime):
+    """Get the start of the week relative to a given datetime."""
+    first_day = date_ - timedelta(date_.isoweekday())
+    return get_day_start(first_day)
+
+
+def get_week_end(date_: datetime):
+    """Get the start of the week relative to a given datetime."""
+    last_day = date_ + timedelta(6 - date_.isoweekday())
+    return get_day_end(last_day)
+
+
+def get_month_start(date_: datetime):
+    """Get the start of the month relative to a given datetime."""
+    first_day = date_ - timedelta(date_.day - 1)
+    return get_day_start(first_day)
+
+
+def get_month_end(date_: datetime):
+    """Get the start of the month relative to a given datetime."""
+    date_ -= timedelta(date_.day - 1)
+    date_ += relativedelta(months=1)
+    last_day = date_ - timedelta(1)
+    return get_day_end(last_day)
+
+
+def get_year_start(date_: datetime):
+    """Get the start of the year relative to a given datetime."""
+    date_ -= timedelta(date_.day - 1)
+    first_day = date_ - relativedelta(months=date_.month - 1)
+    return get_day_start(first_day)
+
+
+def get_year_end(date_: datetime):
+    """Get the start of the year relative to a given datetime."""
+    date_ -= timedelta(date_.day - 1)
+    date_ += relativedelta(months=13 - date_.month)
+    last_day = date_ - timedelta(1)
+    return get_day_end(last_day)
--- date_range_field_template/tests/test_date_interval.py
+++ date_range_field_template/tests/test_date_interval.py
@@ -1,87 +1,119 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from datetime import date, datetime
+import pytz
+
+from datetime import datetime
+from freezegun import freeze_time
+from odoo.tests.common import TransactionCase
 
 from ..date_range import (
-    BasicRelativeDateComputer,
-    MonthStartDateComputer,
-    MonthEndDateComputer,
-    YearStartDateComputer,
-    YearEndDateComputer,
-    DayStartDateComputer,
-    DayEndDateComputer,
+    get_day_start,
+    get_day_end,
+    get_week_start,
+    get_week_end,
+    get_month_start,
+    get_month_end,
+    get_year_start,
+    get_year_end,
 )
 
 
-class TestBasicDateRangeComputer:
+class TestBasicDateConversionCases:
 
     def setup_method(self, method):
         self.today = datetime(2018, 5, 10)
         self.now = datetime(2018, 5, 10, 13, 50, 39, 725000)
 
-    def test_range_in_year_only(self):
-        computer = BasicRelativeDateComputer(years=-3)
-        assert computer.compute(self.today) == datetime(2015, 5, 10)
+    def test_get_day_start(self):
+        assert get_day_start(self.now) == datetime(2018, 5, 10, 0, 0, 0, 0)
 
-    def test_range_in_month_only(self):
-        computer = BasicRelativeDateComputer(months=-6)
-        assert computer.compute(self.today) == datetime(2017, 11, 10)
+    def test_get_day_end(self):
+        assert get_day_end(self.now) == datetime(2018, 5, 10, 23, 59, 59, 999999)
 
-    def test_range_in_days_only(self):
-        computer = BasicRelativeDateComputer(days=-12)
-        assert computer.compute(self.today) == datetime(2018, 4, 28)
+    def test_get_week_start(self):
+        assert get_week_start(self.today) == datetime(2018, 5, 6)
 
-    def test_range_in_weeks_only(self):
-        computer = BasicRelativeDateComputer(weeks=-2)
-        assert computer.compute(self.today) == datetime(2018, 4, 26)
+    def test_get_week_end(self):
+        assert get_week_end(self.today) == datetime(2018, 5, 12, 23, 59, 59, 999999)
 
-    def test_range_in_month_with_beginning_of_month(self):
-        basic_compter = BasicRelativeDateComputer(months=-1)
-        computer = MonthStartDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2018, 4, 1)
+    def test_get_month_start(self):
+        assert get_month_start(self.today) == datetime(2018, 5, 1)
 
-    def test_range_two_months_with_beginning_of_month(self):
-        basic_compter = BasicRelativeDateComputer(months=-2)
-        computer = MonthStartDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2018, 3, 1)
+    def test_get_month_end(self):
+        assert get_month_end(self.today) == datetime(2018, 5, 31, 23, 59, 59, 999999)
 
-    def test_range_in_month_with_end_of_month(self):
-        basic_compter = BasicRelativeDateComputer(months=-1)
-        computer = MonthEndDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2018, 4, 30)
+    def test_get_year_start(self):
+        assert get_year_start(self.today) == datetime(2018, 1, 1)
 
-    def test_range_two_months_with_end_of_month(self):
-        basic_compter = BasicRelativeDateComputer(months=-2)
-        computer = MonthEndDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2018, 3, 31)
+    def test_get_year_end(self):
+        assert get_year_end(self.today) == datetime(2018, 12, 31, 23, 59, 59, 999999)
 
-    def test_range_in_year_with_beginning_of_year(self):
-        basic_compter = BasicRelativeDateComputer(years=-1)
-        computer = YearStartDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2017, 1, 1)
 
-    def test_range_two_years_with_beginning_of_year(self):
-        basic_compter = BasicRelativeDateComputer(years=-2)
-        computer = YearStartDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2016, 1, 1)
+class TestComputedFieldDateRange(TransactionCase):
 
-    def test_range_in_year_with_end_of_year(self):
-        basic_compter = BasicRelativeDateComputer(years=-1)
-        computer = YearEndDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2017, 12, 31)
+    def _get_range(self, reference):
+        range_ = self.ref('date_range_field_template.range_{ref}'.format(ref=reference))
+        return range_.with_context(tz='UTC')
 
-    def test_range_two_years_with_end_of_year(self):
-        basic_compter = BasicRelativeDateComputer(years=-2)
-        computer = YearEndDateComputer(basic_compter)
-        assert computer.compute(self.today) == datetime(2016, 12, 31)
+    def test_previous_month_from_february_of_bisextile_year(self):
+        range_ = self.env.ref('date_range_field_template.range_previous_month')
 
-    def test_range_in_year_with_start_of_day(self):
-        basic_compter = BasicRelativeDateComputer(years=-1)
-        computer = DayStartDateComputer(basic_compter)
-        assert computer.compute(self.now) == datetime(2017, 5, 10, 0, 0, 0, 0)
+        expected_date_min = pytz.utc.localize(datetime(2016, 1, 1))
+        expected_date_max = pytz.utc.localize(datetime(2016, 1, 31, 23, 59, 59, 999999))
 
-    def test_range_in_year_with_end_of_day(self):
-        basic_compter = BasicRelativeDateComputer(years=-1)
-        computer = DayEndDateComputer(basic_compter)
-        assert computer.compute(self.now) == datetime(2017, 5, 10, 23, 59, 59, 999999)
+        with freeze_time(datetime(2016, 2, 29)):
+            assert range_.get_date_min() == expected_date_min
+            assert range_.get_date_max() == expected_date_max
+
+    def test_previous_month_from_february_of_non_bisextile_year(self):
+        range_ = self.env.ref('date_range_field_template.range_previous_month')
+
+        expected_date_min = pytz.utc.localize(datetime(2017, 1, 1))
+        expected_date_max = pytz.utc.localize(datetime(2017, 1, 31, 23, 59, 59, 999999))
+
+        with freeze_time(datetime(2017, 2, 28)):
+            assert range_.get_date_min() == expected_date_min
+            assert range_.get_date_max() == expected_date_max
+
+    def test_previous_year_from_31_december_of_bissextile_year(self):
+        range_ = self.env.ref('date_range_field_template.range_previous_year')
+
+        expected_date_min = pytz.utc.localize(datetime(2015, 1, 1))
+        expected_date_max = pytz.utc.localize(datetime(2015, 12, 31, 23, 59, 59, 999999))
+
+        with freeze_time(datetime(2016, 12, 31)):
+            assert range_.get_date_min() == expected_date_min
+            assert range_.get_date_max() == expected_date_max
+
+    def test_previous_year_from_31_december_of_non_bissextile_year(self):
+        range_ = self.env.ref('date_range_field_template.range_previous_year')
+
+        expected_date_min = pytz.utc.localize(datetime(2016, 1, 1))
+        expected_date_max = pytz.utc.localize(datetime(2016, 12, 31, 23, 59, 59, 999999))
+
+        with freeze_time(datetime(2017, 12, 31)):
+            assert range_.get_date_min() == expected_date_min
+            assert range_.get_date_max() == expected_date_max
+
+    def test_today_from_3_oclock_in_utc_minus_4(self):
+        range_ = self.env.ref('date_range_field_template.range_today')
+        tz_string = 'Canada/Eastern'
+        tz = pytz.timezone(tz_string)
+        expected_date_min = tz.localize(datetime(2018, 5, 9, 0))
+        expected_date_max = tz.localize(datetime(2018, 5, 9, 23, 59, 59, 999999))
+
+        with freeze_time(datetime(2018, 5, 10, 3)):
+            assert range_.with_context(tz=tz_string).get_date_min() == expected_date_min
+            assert range_.with_context(tz=tz_string).get_date_max() == expected_date_max
+
+    def test_today_from_4_oclock_in_utc_minus_4(self):
+        range_ = self.env.ref('date_range_field_template.range_today')
+        tz_string = 'Canada/Eastern'
+        tz = pytz.timezone(tz_string)
+        expected_date_min = tz.localize(datetime(2018, 5, 10, 0))
+        expected_date_max = tz.localize(datetime(2018, 5, 10, 23, 59, 59, 999999))
+
+        with freeze_time(datetime(2018, 5, 10, 4)):
+            assert range_.with_context(tz=tz_string).get_date_min() == expected_date_min
+            assert range_.with_context(tz=tz_string).get_date_max() == expected_date_max

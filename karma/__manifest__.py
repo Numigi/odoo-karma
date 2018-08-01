9327419da83b9ca6c37f9f4d325c394efd2e535d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2018-08-01 15:45:51 -0400

    [11.0] add safe condition evaluation (#2)
    * [11.0] add safe condition evaluation

---
+++ karma/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Karma',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Compute scores on all types of records.',
+    'depends': ['base'],
+    'data': [
+        'data/date_range.xml',
+    ],
+    'installable': True,
+    'application': True,
+}

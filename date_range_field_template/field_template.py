a3a526554b0387c9aa1ed8ba73955c7fd9ae48e5
David Dufresne <david.dufresne@numigi.net>
2018-11-19 10:10:53 -0500

    Allow computed fields to be defined per related model.
    Computed field created from a template can have a new parameter  which allows
    to filter the aggregation on a specific model.

--- date_range_field_template/field_template.py
+++ date_range_field_template/field_template.py
@@ -18,6 +18,7 @@ class ComputedFieldTemplate(models.Model):
     reference = fields.Char(required=True)
     field_type = fields.Selection(FIELD_TYPES, 'Field Type', required=True)
     field_ids = fields.One2many('computed.field', 'template_id', 'Fields')
+    related_model_argument = fields.Boolean('Enable Related Model')
 
     _sql_constraints = [
         ('unique_reference', 'unique(reference, model_id)',
--- date_range_field_template/tools.py
+++ date_range_field_template/tools.py
@@ -2,12 +2,19 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
-def get_technical_field_name(template_reference, range_reference):
+def get_technical_field_name(template_reference, range_reference, related_model: str = None):
     """Get a technical field name given the template and date range references.
 
     :param template_reference: the technical reference of the field template
     :param range_reference: the technical reference of the date range
+    :param related_model: the name of the related model to use for the computation
     :returns: the technical name of the field
     """
-    return 'x__{template}__{range}'.format(
-        template=template_reference, range=range_reference)
+    technical_name = 'x__{template}'.format(template=template_reference)
+
+    if related_model:
+        technical_name += '__{model}'.format(model=related_model.replace('.', '_'))
+
+    technical_name += '__{range}'.format(range=range_reference)
+
+    return technical_name

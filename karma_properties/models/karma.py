2b99e8a1718e8fe67019559b053601a37a3f0949
majouda <majdaelmariouli@gmail.com>
2023-11-15 16:21:31 -0500

    TA#55252 [ADD] Add karma properties

--- karma_properties/models/karma.py
+++ karma_properties/models/karma.py
@@ -11,11 +11,28 @@ class Karma(models.Model):
 
     _inherit = 'karma'
 
-    show_properties = fields.Boolean("Show properties", default=False)
-    display_grade = fields.Boolean("Show grade")
-    display_score = fields.Boolean("Show score")
+    show_properties = fields.Boolean("Show properties", copy=False)
+    display_grade = fields.Boolean("Show grade", copy=False)
+    display_score = fields.Boolean("Show score", copy=False)
+
+    def _check_if_field_exist(self, model_id, name):
+        field_exist = self.env['ir.model.fields'].search([
+            ('model_id', '=', model_id.id), ('name', '=', name)
+            ])
+        return True if field_exist else False
+
+    @api.onchange('model_id')
+    def onchange_model_id(self):
+        if self.model_id and self.show_properties:
+            self.write({
+                'show_properties': False,
+                'display_grade': False,
+                'display_score': False,
+            })
 
     def create_new_field(self, name, type, model_id):
+        if self._check_if_field_exist(model_id, name):
+            return
         new_field = {
             'name': name,
             'field_description': "%s %s" % (self.label, name.split("_")[2].capitalize()),
@@ -35,6 +52,8 @@ class Karma(models.Model):
         view_ids = self.property_view_get(key)
         if view_ids:
             return
+        if not self._check_if_field_exist(self.model_id, field):
+            return
         view_ids = self.env['ir.ui.view'].search([
             ('model', '=', self.model_id.model),
             ('type', '=', view_type),
@@ -50,10 +69,10 @@ class Karma(models.Model):
                              """ % field
             if view_type == "search":
                 label = _("%s %s" % (self.label, field.split("_")[2]))
-                group_by = """ 
+                group_by = """
                             <filter string="%s" name="%s" domain="[]" context="{'group_by':'%s'}"/>
                         """ % (label, field, field)
-                arch_base = arch_base.split("</xpath>",1)[0] + group_by + "</xpath>"
+                arch_base = arch_base.split("</xpath>", 1)[0] + group_by + "</xpath>"
             value = {
                     'name': 'karma.%s.%s' % (field, view_id.name),
                     'type': view_type,
@@ -76,8 +95,8 @@ class Karma(models.Model):
         view_ids = self.property_view_get(key)
         view_ids.sudo().unlink()
 
-    def show_property(self, vals, field):
-        if vals:
+    def add_remove_property(self, property, field):
+        if property:
             self.add_field_to_view(field=field, view_type='tree')
             self.add_field_to_view(field=field, view_type='search')
         else:
@@ -85,10 +104,16 @@ class Karma(models.Model):
 
     def write(self, vals):
         super().write(vals)
-        if "display_grade" in vals:
-            self.show_property(vals['display_grade'], field='x_karma_grade')
-        if "display_score" in vals:
-            self.show_property(vals['display_score'], field='x_karma_score')
+        for rec in self:
+            if self.show_properties:
+                if "display_grade" in vals or "model_id" in vals:
+                    display_grade = "display_grade" in vals or self.display_grade
+                    rec.add_remove_property(display_grade,
+                                            field='x_karma_grade')
+                if "display_score" in vals or "model_id" in vals:
+                    display_score = "display_score" in vals or self.display_score
+                    rec.add_remove_property(display_score,
+                                            field='x_karma_score')
         return True
 
     @api.model
@@ -98,9 +123,3 @@ class Karma(models.Model):
             record.x_karma_score = score.score
             record.x_karma_grade = score.grade
         return score
-
-
-
-
-
-
